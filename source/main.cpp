a86f49f103c9ff50850eff4b937e8c9319a4d05e
acanois <david.richter42@gmail.com>
2025-07-01 14:27:10 -0500

    move osc stuff to its own class

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,8 @@
 #include <lo/lo.h>
 #include <lo/lo_cpp.h>
 
+#include "osc_handler.h"
+
 
 Camera3D camera = {};
 Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
@@ -57,35 +59,9 @@ void draw() {
     }
 }
 
-void initOsc() {
-    lo::ServerThread st(9000);
-    if (!st.is_valid()) {
-        throw std::runtime_error("Failed to initialize OSC server");
-    }
-
-    st.set_callbacks([&st](){printf("Thread init: %p.\n", &st);},
-                 [](){printf("Thread cleanup.\n");});
-
-    std::cout << "URL: " << st.url() << std::endl;
-
-    std::atomic<int> received(0);
-
-    st.add_method("/midi_note", "i",
-                  [&received](lo_arg **argv, int) {
-                      std::cout << "midi_note (" << (++received) << "): " << argv[0]->i << std::endl;
-                  }
-    );
-
-    st.start();
-
-    /*
-     * Send some messages to the server we just created on localhost.
-     */
-    lo::Address a("localhost", "9000");
-}
-
 int main() {
-    initOsc();
+    OscHandler::initOsc();
+
     init();
     draw();
     CloseWindow();
--- source/osc_handler.h
+++ source/osc_handler.h
@@ -8,5 +8,33 @@
 #include <lo/lo_cpp.h>
 
 class OscHandler {
+public:
+    OscHandler() = default;
 
+    static void initOsc() {
+        lo::ServerThread st(9000);
+        if (!st.is_valid()) {
+            throw std::runtime_error("Failed to initialize OSC server");
+        }
+
+        st.set_callbacks([&st](){printf("Thread init: %p.\n", &st);},
+                     [](){printf("Thread cleanup.\n");});
+
+        std::cout << "URL: " << st.url() << std::endl;
+
+        std::atomic<int> received(0);
+
+        st.add_method("/midi_note", "i",
+                      [&received](lo_arg **argv, int) {
+                          std::cout << "midi_note (" << (++received) << "): " << argv[0]->i << std::endl;
+                      }
+        );
+
+        st.start();
+
+        /*
+         * Send some messages to the server we just created on localhost.
+         */
+        lo::Address a("localhost", "9000");
+    }
 };
