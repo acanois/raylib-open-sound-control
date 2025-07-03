897e3e05ba209e8c4115da609ca199fbcb4da7ff
acanois <david.richter42@gmail.com>
2025-07-03 13:55:42 -0500

    remove osc class

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,36 @@
-#include "osc_handler.h"
+#include <iostream>
+#include <lo/lo.h>
+#include <lo/lo_cpp.h>
+
 #include "scene.h"
 
-
 int main() {
-    const auto scene = std::make_unique<Scene>();
-    OscHandler::initOsc();
+    constexpr auto port = 9000;
+    lo::ServerThread st(port);
 
-    scene->update();
+    if (!st.is_valid()) {
+        throw std::runtime_error("Failed to initialize OSC server");
+    }
+
+    st.set_callbacks(
+        [&st](){printf("Thread init: %p.\n", &st);},
+        [](){printf("Thread cleanup.\n");}
+    );
+
+    std::atomic<int> received(0);
+
+    st.add_method(
+        "/midi_note",
+        "i",
+        [&received] (lo_arg **argv, int) {
+            std::cout << "midi_note (" << (++received) << "): " << argv[0]->i << std::endl;
+        }
+    );
+
+    st.start();
+
+    const auto scene = std::make_unique<Scene>();
+
     scene->draw();
 
     return 0;
